e5d035013e38bf23b15a2dc62d918009ac9322b3
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2023-11-26 00:14:50 +0300

    feat: navbar based desing and user profile ico added.

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,29 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
- function Navbar() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Pair 4 Rent A Car 
-         </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+import "./Navbar.css";
 
-          <Link to="/addcar" className="btn btn-outline-light" >Kullanıcı Ekle</Link>
+function Navbar() {
+  return (
+    <div className="container justify-content-center sticky-top bg-body-tertiary p-0">
+      <div class="nav-bar page">
+        <div class="text-center">
+          <div class="row justify-content-md-center m-auto">
+            <div class="col-md-2">
+              <Link to="/" class="text-light text-underline-none wrap-text">
+                <img class="logoImg" alt="App_logo" width="115px" height="105px" />
+              </Link>
+            </div>
+            <div class="col-md-7 mt-auto mb-auto">
+              <ul class="nav justify-content-end">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/">Active</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/addCar">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/car">mamut</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-2 d-flex justify-content-end mt-auto mb-auto me-5">
+              <Link to="/login" class="text-light text-underline-none wrap-text">
+                <img class="userImg" alt="User_Img" />
+              </Link>
+            </div>
+          </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </div >
   );
 }
 
